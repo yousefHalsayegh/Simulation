a42385bd921bdd9f452ddfa8ebe7a2d1aebddca5
Yousef-Alsayegh <96050534+yousefHalsayegh@users.noreply.github.com>
2023-12-21 20:35:02 +0300

    Simple Interface

--- sim/__init__.py
+++ sim/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+from markdown import markdown
 
 def create_app(test_config =None):
     app = Flask(__name__)
         
     # a simple page that says hello
     @app.route('/')
-    def hello():
-        return render_template('landing.html')
+    def index():
+        file = open('ReadME.md', 'r')
+
+        rm = markdown(file.read())
+        return render_template('landing.html', rm=rm)
 
     return app
