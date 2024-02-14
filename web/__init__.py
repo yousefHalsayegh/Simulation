fec9ff62135c0ac386e8e3194cab88b4df218266
Yousef-Alsayegh <96050534+yousefHalsayegh@users.noreply.github.com>
2024-02-14 22:11:37 +0300

    updated multiple files
    Added a landing page for the algorithms.
    Updated the main algorithm page
    Added placeholder text for the ,anding page

--- web/__init__.py
+++ web/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from markdown import markdown
 
 def create_app(test_config =None):
     app = Flask(__name__)
@@ -7,14 +6,16 @@ def create_app(test_config =None):
     # a simple page that says hello
     @app.route('/')
     def index():
-        file = open('web\content\landing.md', 'r')
-
-        rm = markdown(file.read())
-        return render_template('landing.html', rm=rm)
+        return render_template('landing.html')
     
     @app.route('/algorithms')
     def algorithm():
 
+        return render_template('algo_landing.html')
+    
+    @app.route('/algorithms/<algo>')
+    def models(algo):
+        
         return render_template('algo_page.html')
 
     return app
